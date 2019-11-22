f1fdebfac645ce534bde9310fe7b7f66cf1e2ffa
Wojciech <wojt984@o2.pl>
2019-11-22 21:52:23 +0100

    Zadanie 2 -Mulitiplication Table

---
+++ src/pl/coderstrust/main/Main.java
@@ -0,0 +1,15 @@
+package pl.coderstrust.main;
+
+import pl.coderstrust.multiplicationtable.MultiplicationTable;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("This program displays a multiplication table.");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        MultiplicationTable.printMultiplicationTable(size);
+    }
+}
