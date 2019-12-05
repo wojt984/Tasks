ebb998e88da604f9a67328cc1406a3a839adbfe8
[Wojtek] <wojt984@o2.pl>
2019-12-05 16:46:04 +0100

    Zadanie 2 -Mulitiplication Table

--- src/pl/coderstrust/multiplicationtable/MultiplicationTable.java
+++ src/pl/coderstrust/multiplicationtable/MultiplicationTable.java
@@ -1,24 +1,39 @@
 package pl.coderstrust.multiplicationtable;
 
-public class MultiplicationTable {
-    public static void printMultiplicationTable(int size) {
+import static java.lang.String.*;
+
+class MultiplicationTable {
+    public static void main(String[] args) {
+        printMultiplicationTable(12);
+    }
+    static void printMultiplicationTable(int size) {
+        StringBuilder table = new StringBuilder();
+        table.append(format("%4s", ""));
+        for (int j = 1; j <= size; j++) {
+            table.append(format("%4d", j));
+        }
+        table.append("\n");
+        for (int i = 1; i <= size; i++) {
+            table.append(format("%4d",i));
+            for (int k = 1; k <= size; k++) {
+               table.append(format("%4d", i*k));
+            }
+            table.append("\n");
+        }
+        System.out.println(table);
+    }
+
+    private static int calculateColumnWidth(int size) {
         int colLength = 1;
         int maxValue = (size * size);
         while (maxValue >= 10) {
             colLength++;
             maxValue /= 10;
         }
-        for (int i = 0; i <= size; i++) {
-            String line = String.format("%1$"+colLength+ "s", "") + " ";
-            if (i!=0) line =  String.format("%1$"+colLength+ "s", String.valueOf(i)) + " ";
-            for (int j = 1; j <= size; j++) {
-                if (i == 0) {
-                    line += String.format("%1$"+colLength+ "s", String.valueOf(j)) + " ";
-                } else {
-                    line += String.format("%1$"+colLength + "s", String.valueOf(i*j)) + " ";
-                }
-            }
-            System.out.println(line);
-        }
+        return colLength;
+    }
+
+    private static String buildLine( String str) {
+        return format("%4s", str);
     }
 }
